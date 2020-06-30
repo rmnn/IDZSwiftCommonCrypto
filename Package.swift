e5183b8aaa746039e7237f109cc32a9e3ea025cd
Denis Ageev <dageev@openwaygroup.com>
2020-06-30 14:26:43 +0300

    Supported Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -2,6 +2,12 @@ import PackageDescription
 
 let package = Package(
     name: "IDZSwiftCommonCrypto",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_12),
+        .tvOS(.v10),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "IDZSwiftCommonCrypto",
